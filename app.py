5a361079046b0d87ead486801851cb3b3d27829c
makcsoop <makc.roslov@gmail.com>
2023-04-17 20:50:16 +0300

    контакты и большая карта

--- app.py
+++ app.py
@@ -5,17 +5,21 @@ app = Flask(__name__)
 frame_r = 0
 news_flag = False
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/news')
 def news():
     return render_template('news.html')
 
+
 @app.route('/about')
 def company():
     return render_template('company.html')
+
 
 if __name__ == '__main__':
     app.debug = True
