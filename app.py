4c02de3b8e781ae41edec1a64ce01f6efc36eb17
makcsoop <makc.roslov@gmail.com>
2023-04-18 15:06:11 +0300

    рабочие верхние кнопки

--- app.py
+++ app.py
@@ -21,6 +21,31 @@ def company():
     return render_template('company.html')
 
 
+@app.route('/reporting')
+def reporting():
+    return render_template('reporting.html')
+
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+
+@app.route('/questions')
+def questions():
+    return render_template('questions.html')
+
+
+@app.route('/legislation')
+def legislation():
+    return render_template('legislation.html')
+
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
--- static/main.js
+++ static/main.js
@@ -8,4 +8,19 @@
     document.getElementById('about_button').onclick = () => {
         document.location = 'about';
     }
+    document.getElementById('reporting_button').onclick = () => {
+        document.location = 'reporting';
+    }
+    document.getElementById('services_button').onclick = () => {
+        document.location = 'services';
+    }
+    document.getElementById('questions_button').onclick = () => {
+        document.location = 'questions';
+    }
+    document.getElementById('legislation_button').onclick = () => {
+        document.location = 'legislation';
+    }
+    document.getElementById('gallery_button').onclick = () => {
+        document.location = 'gallery';
+    }
 }
